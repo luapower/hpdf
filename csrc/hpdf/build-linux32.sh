5173569ad3972687e2c3950ce09eb0f12f10ed8a
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:51:00 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/hpdf/build-linux32.sh
+++ csrc/hpdf/build-linux32.sh
@@ -1,1 +1,1 @@
-gcc $CFLAGS src/*.c -shared -o ../../bin/linux32/libhpdf.so -Iinclude -I../libpng -I../zlib -L../../bin/linux32 -lz -lpng
+gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/linux32/libhpdf.so -Iinclude -I../libpng -I../zlib -L../../bin/linux32 -lz -lpng
--- csrc/hpdf/build-mingw32.sh
+++ csrc/hpdf/build-mingw32.sh
@@ -1,1 +1,1 @@
-gcc $CFLAGS src/*.c -shared -o ../../bin/mingw32/hpdf.dll -Iinclude -I../libpng -I../zlib -L../../bin/mingw32 -lz -lpng
+gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/mingw32/hpdf.dll -Iinclude -I../libpng -I../zlib -L../../bin/mingw32 -lz -lpng
