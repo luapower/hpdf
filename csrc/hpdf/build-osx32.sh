d3c89ccd93f5c8bef2b9cca010e76e752aeffef5
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:27 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/hpdf/build-osx32.sh
+++ csrc/hpdf/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 src/*.c -shared -o ../../bin/osx32/libhpdf.dylib -Iinclude -I../libpng -I../zlib -L../../bin/osx32 -lz -lpng
+gcc -arch i386 -O2 src/*.c -shared -install_name @loader_path/libhpdf.dylib -o ../../bin/osx32/libhpdf.dylib -Iinclude -I../libpng -I../zlib -L../../bin/osx32 -lz -lpng
--- csrc/hpdf/build-osx64.sh
+++ csrc/hpdf/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 src/*.c -shared -o ../../bin/osx64/libhpdf.dylib -Iinclude -I../libpng -I../zlib -L../../bin/osx64 -lz -lpng
+gcc -arch x86_64 -O2 src/*.c -shared -install_name @loader_path/libhpdf.dylib -o ../../bin/osx64/libhpdf.dylib -Iinclude -I../libpng -I../zlib -L../../bin/osx64 -lz -lpng
