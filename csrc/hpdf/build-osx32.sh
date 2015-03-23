96f5ff48b1f9aef134baca059b2a7b37d4d6ac85
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:02:19 +0200

    unimportant

--- csrc/hpdf/build-osx32.sh
+++ csrc/hpdf/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libhpdf.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libhpdf.dylib" \
 	D=libhpdf.dylib A=libhpdf.a ./build.sh
